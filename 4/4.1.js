5048c59aaa9c5cc305730bf738d7c2f91874cbc8
Karina Delcheva <karina.delcheva@ft.com>
2023-12-04 10:22:43 +0200

    4.1. cleanup

--- 4/4.1.js
+++ 4/4.1.js
@@ -1,10 +1,5 @@
 const fs = require("fs");
 
-function getGameID(line) {
-  // sample: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-  const number = Number(line.split(":")[0].split(" ")[1]);
-}
-
 function calcGamePoints(line) {
   line = line.split(":").pop();
   const winningNumbers = line
@@ -17,16 +12,10 @@ function calcGamePoints(line) {
     .split(" ")
     .filter((number) => number.length > 0)
     .map((number) => Number(number));
-  console.log("winningNumbers", winningNumbers, "elfsNumbers", elfsNumbers);
   let scoreNumbers = elfsNumbers.filter((number) =>
     winningNumbers.includes(number)
   );
-  console.log(
-    "scoreNumbers",
-    scoreNumbers,
-    "scoreNumbers.length",
-    scoreNumbers.length
-  );
+
   if (scoreNumbers.length > 0) {
     return scoreNumbers
       .slice(1)
@@ -34,18 +23,14 @@ function calcGamePoints(line) {
   } else return 0;
 }
 
-// Function to read the file and calculate the possible dame IDs' sum
+
 function calculateResult(filename) {
   try {
     const data = fs.readFileSync(filename, "utf8");
     const lines = data.split("\n").filter((line) => line.length > 0);
     let pointsSum = 0;
-    let gameCopies = 0;
     lines.forEach((line) => {
-      if (calcGamePoints(line) > 0) {
-        winningGames.push(getGameID(line));
         pointsSum += calcGamePoints(line);
-      }
     });
 
     return pointsSum;
@@ -55,7 +40,6 @@ function calculateResult(filename) {
   }
 }
 
-// Replace 'filename.txt' with the actual filename containing the puzzle data
 const filename = "input.txt";
 const result = calculateResult(filename);
 
