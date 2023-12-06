eb55fece4384e5c45b75f046184bae7385440735
Karina Delcheva <karina.delcheva@ft.com>
2023-12-06 14:50:32 +0200

    Day 6: part 2

---
+++ 6/6.2.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+function calculateResult(filename) {
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    let lines = data.split("\n").filter((line) => line.length > 0);
+    lines =lines.map(line => line.split(":")[1].trim().split(' ')).map(line => Number(line.filter(num => num.length > 0).join('')))
+    let [time, distance] = lines
+    console.log(time, distance)
+    let margin = 1
+
+    n = 0
+    for (let hold = 1; hold < time; hold++) {
+        if (hold * (time - hold) > distance) {
+            n += 1
+        }
+    }
+    margin*=n
+    
+    return margin;
+  } catch (error) {
+    console.error("Error reading the file:", error.message);
+    return 0;
+  }
+}
+
+const filename = "input.txt";
+const result = calculateResult(filename);
+
+console.log("Sum of races:", result);
